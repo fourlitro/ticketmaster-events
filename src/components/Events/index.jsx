a900188b0f358538c907619f59089b6507614148
Alexis Suarez <a219201031@unison.mx>
2023-11-23 23:28:33 -0700

    pasando eventos como props

--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -10,13 +10,19 @@ import eventsJSON from "../../data/events.json";
 const Events = () => {
     const [data] = useState(eventsJSON);
     const { _embedded: { events } } = data;
-    
+
+    const handleEventItemClick = (id) => {
+        console.log("evento clickeado: ", id)
+    }
+
     const eventsComponent = events.map((eventItem) => (
         <EventItem 
             key={`event-item-${eventItem.id}`} 
             name={eventItem.name}
             info={eventItem.info}
             image={eventItem.images[0].url} 
+            onEventClick={handleEventItemClick}
+            id={eventItem.id}
         />
     ));
 
