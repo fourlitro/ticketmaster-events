1574ebe6ca5f040d3c47c5487c71b027dc2ed81e
Alexis Suarez <a219201031@unison.mx>
2023-11-23 23:19:54 -0700

    Manejando eventos

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 const Navbar = () => {
+
+    const [search, setSearch] = useState('');
+
+    const handleInputChange = (event) => {
+        setSearch(event.target.value)
+    };
+
+    console.log(search)
+
     return (
         <div>
             <p>Mi boletera</p>
-            <input placeholder="busca tu evento favorito"/>
+            <input 
+            placeholder="busca tu evento favorito" 
+            onChange={handleInputChange}
+            value={search}
+            />
         </div>
     );
 };
