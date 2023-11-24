4c2a4553ebf2736f003523458b1dd3e72a3f5358
Alexis Suarez <a219201031@unison.mx>
2023-11-23 23:39:47 -0700

    Event Bubling: Hacer que los eventos no se propagen hacia los padres

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,10 +1,16 @@
-const EventItem = ({ info, id, name, image, onEventClick}) => {
+const EventItem = ({ info, id, name, image, onEventClick }) => {
+
+    const handleSeeMoreClick = (event) =>{
+        event.stopPropagation();
+        onEventClick(id)
+    };
+
     return (
-        <div>
-            <img src={image} alt={name} width={200} height={200}/>
+        <div onClick={() => console.log("padre clickeado")}>
+            <img src={image} alt={name} width={200} height={200} />
             <h4>{name}</h4>
             <p>{info}</p>
-            <button onClick={() => onEventClick(id)}>Ver mas</button>
+            <button onClick={handleSeeMoreClick}>Ver mas</button>
         </div>
     );
 };
